c3d6feb77919dc63f1f816078d8027134a711d6f
Susan <szhou089@gmail.com>
2022-03-13 21:18:25 -0700

    "adding convert

--- AlgosPractice.js
+++ AlgosPractice.js
@@ -8,3 +8,11 @@ console.log(getSum(3, 2));// ➞ 5
 console.log(getSum(-3, -6));// ➞ -9
 console.log(getSum(7, 3));// ➞ 10
 
+//Write a function that takes an integer minutes and converts it to seconds.
+function convert(num) {
+    return num*60;
+}
+console.log(convert(5));//300
+console.log(convert(3));//180
+console.log(convert(2));//120
+
