aecd805b08815b913139e6d7d13e8c55345cdc72
Susan <szhou089@gmail.com>
2022-03-13 21:13:56 -0700

    "initial commit"

---
+++ AlgosPractice.js
@@ -0,0 +1,10 @@
+//Create a function that takes two numbers as arguments and returns their sum.
+
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(getSum(3, 2));// ➞ 5
+console.log(getSum(-3, -6));// ➞ -9
+console.log(getSum(7, 3));// ➞ 10
+
