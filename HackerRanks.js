fa9ee770a7536437a1466f89e922813d80232afc
ZhouSusan <szhou089@gmail.com>
2022-04-05 21:43:38 -0700

    "adding  arithmetic operators function"

--- HackerRanks.js
+++ HackerRanks.js
@@ -84,3 +84,36 @@ function regexVar(str) {
     let re = RegExp(/^([aeiou]).*\1$/);;
     return re;
 }
+
+
+/**
+ * Complete the following functions in the editor below:
+getArea(length, width): Calculate and return the area of a rectangle having sides length and width.
+getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides length and width.
+The values returned by these functions are printed to stdout by locked stub code in the editor.
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    area = length * width;
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = (2*length) + (2*width);
+    return perimeter;
+}
