57d17c92251bed7752d458c7f0582b79bc106c79
ZhouSusan <szhou089@gmail.com>
2022-04-14 23:50:16 -0700

    "added CountDownBy8 "

--- HackerHero-Basic_Algos.js
+++ HackerHero-Basic_Algos.js
@@ -4,3 +4,13 @@ For example, sum(2,5) should return 7; sum(3,10) should return 13.*/
 function sum(num1, num2){
     return num1 + num2;
 }
+
+//Log positive numbers starting at 2019, counting down by 8.
+
+function CountDownBy8(){
+    for (let i = 2019; i > 0; i-= 8) {
+        console.log(i);
+    }    
+}
+
+CountDownBy8();
