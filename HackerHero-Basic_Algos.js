6f1516f7c076f242933c18390336239aaf460243
ZhouSusan <szhou089@gmail.com>
2022-04-14 23:58:26 -0700

    "added makeItBig"

--- HackerHero-Basic_Algos.js
+++ HackerHero-Basic_Algos.js
@@ -26,3 +26,16 @@ function celciusToFahrenheit(cDegrees) {
 }
 
 console.log(celciusToFahrenheit(32));
+
+/*
+Given an array, write a function that changes all positive numbers in the array to “big”.
+Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1,"big","big",-5].
+*/
+
+function makeItBig(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+        }
+    }
+    return arr;
+}    
