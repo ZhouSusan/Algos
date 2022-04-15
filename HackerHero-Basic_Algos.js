0513678b742400244ed940769fbe35a2937ad9ec
ZhouSusan <szhou089@gmail.com>
2022-04-14 23:54:48 -0700

    "added celciusToFahrenheit"

--- HackerHero-Basic_Algos.js
+++ HackerHero-Basic_Algos.js
@@ -14,3 +14,15 @@ function CountDownBy8(){
 }
 
 CountDownBy8();
+
+/* 
+Kelvin wants to convert between temperature scales. Create celciusToFahrenheit(cDegrees) 
+that accepts a number of degrees in Celcius, and returns the equivalent temperature as expressed in Fahrenheit degrees. 
+For review, Fahrenheit = (9/5 * Celsius) + 32.
+*/
+
+function celciusToFahrenheit(cDegrees) {
+    return ((9/5) * cDegrees) + 32;
+}
+
+console.log(celciusToFahrenheit(32));
