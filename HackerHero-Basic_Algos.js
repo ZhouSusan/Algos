9799f69b25cb274df3f09a6f14696020d48ec216
ZhouSusan <szhou089@gmail.com>
2022-04-14 23:39:55 -0700

    "added sum function"

---
+++ HackerHero-Basic_Algos.js
@@ -0,0 +1,6 @@
+/*Create a function sum() that returns the sum of the two numbers passed as its arguments.
+For example, sum(2,5) should return 7; sum(3,10) should return 13.*/
+
+function sum(num1, num2){
+    return num1 + num2;
+}
