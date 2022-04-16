3c7fe73ab801b2b9097d5879328b2bc8f27eef52
ZhouSusan <szhou089@gmail.com>
2022-04-16 15:45:51 -0700

    "refactored findLeastCommonNumber"

--- coderust.js
+++ coderust.js
@@ -131,8 +131,28 @@ let findLeastCommonNumber = function(arr1, arr2, arr3) {
     return commonSmallesNum;
 };
 
+const findLeastCommonNumber2 = (v1, v2, v3) => {
+    let [i, j, k] = [0, 0, 0];
+
+    while (i < v1.length && j < v2.length && k < v3.length) {
+        if (v1[i] === v2[j] && v2[j] === v3[k]) {
+            return v1[i];
+        }
+
+         //increment the iterator
+        if (v1[i] <= v2[j] && v1[i] <= v3[k]) {
+            i++;
+        } else if (v2[j] <= v1[i] && v2[j] <= v3[k]) {
+            j++;
+        } else if (v3[k] <= v1[i] && v3[k] <= v2[j]) {
+            k++
+        }
+    }
+    return -1;
+}
+
 v1 = [6, 7, 10, 25, 30, 63, 64]
 v2 = [0, 4, 5, 6, 7, 8, 50]
 v3 = [1, 6, 10, 14]
 
-console.log(findLeastCommonNumber(v1, v2, v3));
+console.log(findLeastCommonNumber2(v1, v2, v3));
