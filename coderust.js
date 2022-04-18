5f4649fd146771b1a11844649bdae1b2a4ba8b44
ZhouSusan <szhou089@gmail.com>
2022-04-18 10:31:39 -0700

    "refractored findLowIndex"

--- coderust.js
+++ coderust.js
@@ -160,24 +160,68 @@ console.log(findLeastCommonNumber2(v1, v2, v3));
 //  We’re given a sorted array of integers, nums, and an integer value, target. Return the low and high index of the given target element. 
 //If the indexes are not found, return -1.
 
-let first = 0; 
-let last = 0;
-
 let findLowIndex = function(arr, key) {
   if (arr.indexOf(key) > -1) {
-    first = arr.indexOf(key);
-    return first;
+    return arr.indexOf(key);
   }
   return -1;
 };
 
 let findHighIndex = function(arr, key) {
   if (arr.indexOf(key) > -1) {
-    last = arr.lastIndexOf(key);
-     return last;
+    return arr.lastIndexOf(key);
   }
   return -1;
 };
 
-findLowIndex([1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 9], 5)
-findHighIndex([1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 9], 5)
+console.log(findLowIndex([1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 9], 5));
+console.log(findHighIndex([1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 9], 5));
+
+let findLowIndex2 = function(arr, key) {
+    let low = 0;
+    let high = arr.length-1;
+
+    let mid = Math.floor(high/2);
+    while (low <= high) {
+        if(arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+
+        mid = low + Math.floor((high-low)/2);
+    }
+
+    if (low < arr.length && arr[low] === key) {
+        return low;
+    }
+    return -1;
+  };
+  
+  let findHighIndex2 = function(arr, key) {
+    let low = 0;
+    let high = arr.length-1;
+    let mid = Math.floor(high/2); 
+
+    while (low <= high) {
+        if (arr[mid] <= key) {
+            low = mid + 1; 
+        } else {
+            high = mid - 1
+        }
+
+        mid = low + Math.floor((high-low)/2);
+
+        if (high === -1) {
+            return high;
+        }
+
+        if (high < arr.length && arr[high] === key) {
+            return high;
+        }
+    }
+    return -1;
+  };
+
+console.log(findLowIndex2([1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 9], 2), 
+findHighIndex2([1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 9], 2));
