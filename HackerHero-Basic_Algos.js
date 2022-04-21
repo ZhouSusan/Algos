e71aa41191b5eb64e47e74c0f177a56fc48038f0
ZhouSusan <szhou089@gmail.com>
2022-04-21 16:25:14 -0700

    added threesFives

--- HackerHero-Basic_Algos.js
+++ HackerHero-Basic_Algos.js
@@ -99,3 +99,27 @@ function swapTowardCenter(arr){
 
 return arr;
 }
+
+/*
+Create threesFives(n) that adds values from 1 and n (inclusive) if that value is not divisible by 3 or 5. Return the final sum.
+For example, threesFives(10) returns 22 as it only returns the sum of 1, 2, 4, 7, and 8. 
+On that example, it skips 3, 6, 9 as those are divisible by 3. It also skips 5, and 10 as it's divisible by 5.
+*/
+
+function threesFives(num){
+    let sum = 0;
+ 
+    while (num > 0) {
+
+        if (num % 3 === 0 || num % 5 === 0) {
+            num--;
+            continue;
+        }
+        sum += num;
+        num--;
+
+    }
+    console.log(sum);
+    return sum;
+}
+console.log(threesFives(10))
