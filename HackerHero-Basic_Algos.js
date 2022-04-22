294002481c2c8dbd3e76e3df8ccf578a4ffb104e
ZhouSusan <szhou089@gmail.com>
2022-04-22 05:52:52 -0700

    clockHandAngles

--- HackerHero-Basic_Algos.js
+++ HackerHero-Basic_Algos.js
@@ -204,3 +204,31 @@ sumToOne2(35),
 sumToOne2(928),
 sumToOne2(5798),
 sumToOne2(35798))
+
+/*
+Regardless of how hard a Dojo student works (and they should work hard), they need time now and then to unwind – like hands on a clock. 
+Traditional clocks are increasingly uncommon, but most can still read an analog clock’s hands of hours, minutes and seconds. 
+Create clockHandAngles(seconds) that, given a number of seconds since 12:00:00, returns angles (in degrees) of the hour, minute and second hands. 
+As review, 360 degrees form a full rotation.
+ Have the degree round up as a whole number (e.g. 38 instead of 37.632...)
+
+For input of 3600 secs (equivalent to 1:00:00), have it return "[30, 0, 0]" meaning the hour is 30 degrees, 
+the minute and the second is 0 degree from noon.
+
+Similarly, clockHandAngles(10800) should return [90, 0, 0] as 10,800 seconds is equivalent to 3:00:00.
+*/
+
+function clockHandAngles(seconds) {
+    let counts = [0, 0, 0, seconds];
+    let clockInfo = [3600, 60, 1];
+    //scaling factor to convert from units (minutes, seconds, etc) to degrees,
+    let scaling = [30, 6, 6]; 
+
+    for (let i = 0; i < 3; i++) {
+        counts[i] = Math.ceil(counts[3]/clockInfo[i] * scaling[i] % 360);
+        counts[3] -= Math.floor(counts[i]/scaling[i]) * clockInfo[i];
+    }
+
+return [counts[0], counts[1], counts[2]];
+    
+}
