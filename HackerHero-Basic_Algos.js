9c34b7d54549269ec84413d41213b11692e5ac5c
ZhouSusan <szhou089@gmail.com>
2022-04-24 19:49:40 -0700

    added zipIt

--- HackerHero-Basic_Algos.js
+++ HackerHero-Basic_Algos.js
@@ -429,3 +429,24 @@ function isCreditCardValid(digitArr) {
 
     return total % 10 === 0? true: false;
 }
+
+/*
+Create a standalone function that accepts two arrays and combines their values sequentially into a new array, at alternating indices starting with first array.
+Extra values from either array should be included afterward.
+
+Given [1,2] and [10,20,30,40], return new array containing [1,10,2,20,30,40].
+*/
+
+function zipIt(arr1, arr2) {
+    let newArr = [];
+    for (let i = 0; i < arr1.length || i < arr2.length; i++) {
+        if (i < arr1.length) {
+            newArr.push(arr1[i]);
+        }
+        
+        if (i < arr2.length) {
+            newArr.push(arr2[i]);
+        }
+    }
+    return newArr;
+}
