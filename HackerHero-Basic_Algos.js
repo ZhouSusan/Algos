e17a4ce53bf0c7a2af8d4354a3b22cf47999c0ce
ZhouSusan <szhou089@gmail.com>
2022-04-24 23:09:25 -0700

    added bubbleSort

--- HackerHero-Basic_Algos.js
+++ HackerHero-Basic_Algos.js
@@ -480,3 +480,20 @@ function parensValid(str){
     }
     return true;
 }
+
+function bubbleSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length-i-1); j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
