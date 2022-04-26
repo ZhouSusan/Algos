28d56c49972ecb7c4790b583460ccf34c260aa23
ZhouSusan <szhou089@gmail.com>
2022-04-26 13:21:05 -0700

    added fib

--- HackerHero-DataStructures.js
+++ HackerHero-DataStructures.js
@@ -34,3 +34,21 @@ function factorial(n) {
     
     return n * factorial(n-1); 
 }
+
+/*
+Create a function to generate Fibonacci numbers. In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1. Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).
+
+Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8).
+*/
+
+function fib(n) {
+    if (n <= 0) {
+        return 0;
+    }
+    
+    if (n <= 2) {
+        return 1;
+    }
+    
+    return fib(n-2) + fib(n-1);
+}
