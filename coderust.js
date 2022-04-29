2e83c62b929719a674c2c0bffe6a90e1fde00f31
ZhouSusan <szhou089@gmail.com>
2022-04-28 21:31:59 -0700

    added rotateArray.js

--- coderust.js
+++ coderust.js
@@ -1,29 +1,3 @@
-let rotateArray = function(nums, n) {
-    if (n === 0) {
-        return nums;
-    }
-    
-    if (n < 0) {
-        while (n < 0) {
-            let temp = nums.shift();
-            nums.push(temp);
-            n++;
-        }
-    } else if (n > 0) {
-        while (n > 0) {
-            let temp = nums.pop();
-            nums.unshift(temp);
-            n--;
-        }
-    }
-
-    return nums;
-};
-
-console.log(rotateArray([-1,-100,3,99], 2));
-let arr = [1, 10, 20, 0, 59, 86, 32, 11, 9, 40];
-console.log(rotateArray(arr, -3))
-
 //We’re given a sorted integer array, nums and an integer value, target. The array is rotated by some arbitrary number. 
 //Search the target in this array. If the target does not exist then return -1.
 
