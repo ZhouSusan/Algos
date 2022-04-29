c6577bd6d6e3d78735f194f42b3526b327bd2f80
ZhouSusan <szhou089@gmail.com>
2022-04-28 21:56:20 -0700

    added moveZerosToLeft.js

--- coderust.js
+++ coderust.js
@@ -1,36 +1,3 @@
-/*We’re given an integer array, nums. 
-Move all zeroes if any to the left while maintaining the order of other elements in the array.*/
-
-let moveZerosToLeft = function(nums) {
- 
-     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            nums.splice(i, 1);
-            nums.unshift(0);
-        } 
-      }
-      console.log(nums);
-      return nums;
-  };
-
-console.log(moveZerosToLeft([1, 10, 20, 0, 59, 63, 0, 88, 0]));
-
-// let moveZerosToLeft2 = function(nums) {
-//     if (nums.length < 1) {
-//         return nums;
-//     }
-
-//     let lengthNums = nums.length;
-
-//     //initialize the two pointers
-//     let writeIndex = length - 1;
-//     let readIndex = length - 1;
-
-//     while (readIndex >= 0) {
-
-//     }
-// }
-
 /*
 We’re given an array of daily stock prices (integers for simplicity). 
 Return the buying and selling prices for making the maximum profit.
