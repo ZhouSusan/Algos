53e2ccfc0ddeb8c528403131f955ab27d875c720
ZhouSusan <szhou089@gmail.com>
2022-05-03 22:55:05 -0700

    added started template to Queue.js

---
+++ Data_Structures/Queue/Queue.js
@@ -0,0 +1,10 @@
+class Queue {
+    constructor(items = []) {
+        this.items = items;
+    }
+    // add methods here, outside constructor
+    newMethod(params) {
+        // 'this' keyword will refer to the class instance
+        // that newMethod was called on
+    }
+}
