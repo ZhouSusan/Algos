3ae8c32ca031d2be83afc82c3a105fae2a91cc7f
ZhouSusan <szhou089@gmail.com>
2022-05-20 18:17:01 -0700

    "added multiplicationOf7.js"

---
+++ HackerMoon/multiplicationTableOf7.js
@@ -0,0 +1,7 @@
+function multiplicationOf7() {
+    for (let i = 0; i <= 10; i++) {
+        console.log(`7 * ${i} =`, 7*i);
+    }
+}
+
+multiplicationOf7();
--- HackerMoon/printOddNumLessThan100.js
+++ HackerMoon/printOddNumLessThan100.js
@@ -1,3 +1,5 @@
+//Coding challenge #2: Print the odd numbers less than 100
+
 function  printOddNumLessThan100 () {
     for (let i = 1; i < 101; i+=2) {
         console.log(i);
