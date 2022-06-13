b4d3fe55b0a3b27d299089b7414e225629574006
ZhouSusan <szhou089@gmail.com>
2022-06-13 00:02:15 -0700

    "updated shortenArray.js"

--- Data_Structures/Arrays/shortenArray.js
+++ Data_Structures/Arrays/shortenArray.js
@@ -1,16 +1,14 @@
 function shortenArray(arr) {
     var copyArr = arr.slice();
-     return copyArr.filter(item => item);
-  
-  }
-  
-  var test1 = [1,2,3,4,,,,,];
-  console.log("test 1: ", test1);
-  var test2 = [, , , , "a", true, 4, 4];
-  var test3 = [1, , 2, , 3, , 4];
-  var test4 = [, , , , , , ,]
-  
-  console.log(shortenArray(test1)) //[1,2,3,4]
-  console.log(shortenArray(test2)) //["a", true, 4, 4]
-  console.log(shortenArray(test3)) //[1,2,3,4]
-  console.log(shortenArray(test4));//[]
+    return copyArr.filter(item => item);
+}
+
+let test1 = [1,2,3,4,,,,,];
+let test2 = [, , , , "a", true, 4, 4];
+let test3 = [1, , 2, , 3, , 4];
+let test4 = [, , , , , , ,]
+
+console.log(shortenArray(test1)) //[1,2,3,4]
+console.log(shortenArray(test2)) //["a", true, 4, 4]
+console.log(shortenArray(test3)) //[1,2,3,4]
+console.log(shortenArray(test4));//[]
