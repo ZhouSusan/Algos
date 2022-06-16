087d095ccc2d32d1a62d1b6c110b740fe08a6be8
ZhouSusan <szhou089@gmail.com>
2022-06-15 23:22:57 -0700

    "refactored StackUsingQueuesclass"

--- Data_Structures/Stacks/StackUsingQueue.js
+++ Data_Structures/Stacks/StackUsingQueue.js
@@ -1,26 +1,43 @@
 class StackUsingQueue {
-
     constructor() {
         this.queue1 = [];
         this.queue2 = [];
-    };
-
-    push(data){
-        this.queue1.push(data);
-        return data;
     }
     
+    push(data){
+
+        if (this.queue1.length !== 0) {
+        this.queue1.push(data);
+            for (let i = 0; i < this.queue2.length; i++) {
+                this.queue1.push(this.queue2.pop());
+            }
+        }//if queue2 is empty 
+        else {
+            this.queue2.push(data);
+
+            if (this.queue1.length !== 0) {
+                for (let j = 0; j < this.queue1.length; j++) {
+                    this.queue2.push(this.queue1.pop());
+                }
+            }
+        }
+    }
+
     isEmpty() {
         if (this.queue1.length + this.queue2.length === 0) {
-            return false;
-        } 
-    }
-    
-    pop() {
-        if (this.isEmpty()) {
-            return -1;
+            return true;
         }
-        let first = this.queue2.push(this.queue1.shift());
-        return first;
+        return false;
+    }
+
+    pop() {
+    if (this.isEmpty()) {
+        return -1;
+    }
+        if (this.queue1.length !== 0) {
+            return this.queue1.pop();
+        } else {
+            return this.queue2.pop();
+        }
     }
 }
