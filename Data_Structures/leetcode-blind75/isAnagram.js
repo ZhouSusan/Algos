196885dc39f156937693efbd5b415061b3005f6a
ZhouSusan <szhou089@gmail.com>
2022-06-19 23:20:30 -0700

    "refactored isAnagram "

--- Data_Structures/leetcode-blind75/isAnagram.js
+++ Data_Structures/leetcode-blind75/isAnagram.js
@@ -52,3 +52,32 @@ var isAnagram2 = function(s, t) {
     }
     return true; 
 };
+
+var isAnagram3 = function(s, t) {
+    if (s.length != t.length) {
+        return false; 
+    }
+    
+    let sObj = new Map();
+    
+    for (let i = 0; i < s.length; i++) {
+        if (sObj.has(s[i])) {
+            sObj.set(s[i], sObj.get(s[i])+1);
+        } else {
+            sObj.set(s[i],1);
+        }
+    }
+    
+    for (let j = 0; j < t.length; j++) {
+        if (sObj.has(t[j])) {
+            if (sObj.get(t[j]) == 0) {
+                return false;
+            } else {
+                sObj.set(t[j], sObj.get(t[j])-1);
+            }
+        } else {
+            return false;
+        }
+    }
+    return true; 
+};
