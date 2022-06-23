960b75df221a046a8b0ab79fad09724ca6a6e348
ZhouSusan <szhou089@gmail.com>
2022-06-23 00:06:47 -0700

    "updated canConstruct"

--- Data_Structures/leetcode-blind75/canConstruct.js
+++ Data_Structures/leetcode-blind75/canConstruct.js
@@ -5,6 +5,11 @@
  */
 const canConstruct = function(ransomNote, magazine) {
     let ransomNoteLength = ransomNote.length; 
-    let prefixSlice = magazine.slice(0, ransomNoteLength+ 1);
+    let prefixSlice = magazine.slice(0, ransomNoteLength);
+    console.log(prefixSlice);
     return ransomNote == prefixSlice ? true: false;
 };
+
+console.log(canConstruct('a', 'b'));//false
+console.log(canConstruct('aa', 'ab'));//false
+console.log(canConstruct('aa', 'aab'));//true
