3e77d2f12c8bc72eba281e737841fe1f701343f0
ZhouSusan <szhou089@gmail.com>
2022-06-25 23:35:15 -0700

    "added twoSum in python"

---
+++ Data_Structures/leetcode-blind75/twoSum.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target) {
+    dictionary = {};
+    for i in range(len(nums)):
+        x = nums[i]
+        if target-x in dictionary:
+            return (dictionary[target-x]+1, i + 1)
+        else:
+            dictionary[x] = i
+    return []
+}
