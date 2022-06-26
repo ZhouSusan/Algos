74a5d7a5cc352e08b40938bd086025fef55db708
ZhouSusan <szhou089@gmail.com>
2022-06-25 23:29:40 -0700

    "refactored canConstruct"

--- Data_Structures/leetcode-blind75/canConstruct.js
+++ Data_Structures/leetcode-blind75/canConstruct.js
@@ -1,13 +1,26 @@
-/**
- * @param {string} ransomNote
- * @param {string} magazine
- * @return {boolean}
- */
-const canConstruct = function(ransomNote, magazine) {
-    let ransomNoteLength = ransomNote.length; 
-    let prefixSlice = magazine.slice(0, ransomNoteLength);
-    console.log(prefixSlice);
-    return ransomNote == prefixSlice ? true: false;
+var canConstruct = function(ransomNote, magazine) {
+    let letterCount = new Map();
+    
+    for (let i = 0; i < magazine.length; i++) {
+        if (letterCount.has(magazine[i])) {
+            letterCount.set(magazine[i],            letterCount.get(magazine[i])+1);
+        } else {
+            letterCount.set(magazine[i], 1);
+        }
+    }
+    
+    for (let j = 0; j < ransomNote.length; j++) {
+        if (letterCount.has(ransomNote[j])) {
+            if (letterCount.get(ransomNote[j]) == 0) {
+                return false;
+            } else {
+                letterCount.set(ransomNote[j], letterCount.get(ransomNote[j])-1);
+            }
+        } else {
+        return false;  
+        }
+    }
+    return true;
 };
 
 console.log(canConstruct('a', 'b'));//false
