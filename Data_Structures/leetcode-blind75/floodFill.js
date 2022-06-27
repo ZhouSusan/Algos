6deb96e853579861b34ecb66f7efc9f9a49de143
ZhouSusan <szhou089@gmail.com>
2022-06-27 15:52:03 -0700

    "refactored floodFill"

--- Data_Structures/leetcode-blind75/floodFill.js
+++ Data_Structures/leetcode-blind75/floodFill.js
@@ -6,40 +6,72 @@
  * @return {number[][]}
  */
 
-const floodFill = function(image, sr, sc, color) {
+// const floodFill = function(image, sr, sc, color) {
     
-    const colorToChange = image[sr][sc];
-    if (colorToChange == color) {
+//     const colorToChange = image[sr][sc];
+//     if (colorToChange == color) {
+//         return image;
+//     }
+//     floodFillrec(image,sr,sc,color, colorToChange);
+//     return image;
+// };
+
+// function floodFillrec(image,sr,sc,color, colorToChange) {
+    
+//     let neighborList = [];
+//     if (image[sr-1] != undefined && image[sr-1][sc] == colorToChange) {
+        
+//         neighborList.push([sr-1,sc]);
+//     }
+//     if (image[sr+1] != undefined && image[sr+1][sc] == colorToChange) {
+        
+//         neighborList.push([sr+1,sc]);
+//     }
+//     if (image[sr][sc-1] == colorToChange) {
+        
+//         neighborList.push([sr,sc-1]);
+//     }
+//     if (image[sr][sc+1] == colorToChange) {
+        
+//         neighborList.push([sr,sc+1]);
+//     }
+    
+//     image[sr][sc] = color;
+    
+//     for (const item of neighborList) {
+//         console.log(item);
+//         floodFillrec(image,item[0],item[1],color,colorToChange);
+//     }
+// }
+
+let floodFill = function (image, sr,sc,color) {
+    let oldColor = image[sr][sc];
+
+    if (color == oldColor) {
         return image;
     }
-    floodFillrec(image,sr,sc,color, colorToChange);
-    return image;
-};
 
-function floodFillrec(image,sr,sc,color, colorToChange) {
-    
-    let neighborList = [];
-    if (image[sr-1] != undefined && image[sr-1][sc] == colorToChange) {
-        
-        neighborList.push([sr-1,sc]);
-    }
-    if (image[sr+1] != undefined && image[sr+1][sc] == colorToChange) {
-        
-        neighborList.push([sr+1,sc]);
-    }
-    if (image[sr][sc-1] == colorToChange) {
-        
-        neighborList.push([sr,sc-1]);
-    }
-    if (image[sr][sc+1] == colorToChange) {
-        
-        neighborList.push([sr,sc+1]);
-    }
-    
     image[sr][sc] = color;
-    
-    for (const item of neighborList) {
-        console.log(item);
-        floodFillrec(image,item[0],item[1],color,colorToChange);
+
+    //checking left
+    if (image[sr-1] != undefined && image[sr-1][sc] == oldColor) {
+        floodFill(image, sr-1, sc, color);
     }
+
+    //checking right
+    if (image[sr+1] != undefined && image[sr+1][sc] == oldColor) {
+        floodFill(image, sr+1, sc, color);
+    }
+
+    //checking up
+    if (image[sc-1] != undefined && image[sr][sc-1] == oldColor) {
+        floodFill(image,sr, sc-1, color);
+    }
+
+    //checking down
+    if (image[sc+1] != undefined && image[sr][sc+1] == oldColor) {
+        floodFill(image, sr, sc+1, color);
+    }
+
+    return image;
 }
