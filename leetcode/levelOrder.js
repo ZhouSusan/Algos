36c1731b4d08c8a797f729c60cfc89d7350ab49a
ZhouSusan <szhou089@gmail.com>
2022-08-19 18:00:24 -0700

    "added #102 Binary Tree Level Order Traversal"

--- leetcode/levelOrder.js
+++ leetcode/levelOrder.js
@@ -41,3 +41,44 @@ var levelOrder = function(root) {
     }
     return treeArr;
 };
+
+/*We are going to traversal the level from the tree, left to right, and add in the subLevel, add the node's value from left to right in a separate list 
+We will use Breath First Search on our tree using queue(adding elements to the right and popping elements from the left)
+T: O(n)- visting each node in tree once
+S: O(n)- becuase our queue can have up to o(n/2) elements can be rounded to O(n) with Breath First Search
+*/
+var levelOrder = function(root) {
+    if (root == null)  {
+        return [];
+    }
+    let queue = [];
+    let result = [];
+
+    queue.push(root);
+    
+    //We are going to fun bsf in a while loop
+    //while the q is not empty 
+    while (queue.length != 0) {
+        //this is to loop through one tree level at a time, add the nodes into a sublist and then adding to that list 
+        queueLength = queue.length;
+        let level = [];
+        for (let i = 0; i < queueLength; i++) {
+            //pop nodes from the left of the queue
+            let node = queue.shift();
+            //it is possible that the node is null, 
+            if (node != null) {
+                //if node is not null, add the node to level(array) and add the children to the queue if the children are not null. In the next iteration, it will check if the children node's are not nullble, before adding to to the level array 
+                level.push(node.val);
+                if (node.left != null) {
+                    queue.push(node.left);
+                }
+                if (node.right != null) {
+                    queue.push(node.right);
+                }
+            }
+        }
+        //After we go through each level we are going to add that subArr to our level array
+        result.push(level);
+    }
+    return result; 
+};
