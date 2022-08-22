9b079ff89a09e3b8772c9bb8bd761bdcca42978a
ZhouSusan <szhou089@gmail.com>
2022-08-21 23:52:37 -0700

    "refactored lengthOfLongestSubstring"

--- leetcode/lengthOfLongestSubstring.js
+++ leetcode/lengthOfLongestSubstring.js
@@ -21,3 +21,23 @@ let lengthOfLongestSubstring = function(s) {
     }
     return maxLength;
 };
+
+var lengthOfLongestSubstring2 = function(s) {
+    let longest = 0;
+    let start = 0; 
+    let current = 0;
+    
+    let charSet = new Set();
+    
+    while (current < s.length) {
+        //check if the current character exist in charSet 
+        if (charSet.has(s[current])) {
+            charSet.delete(s[start++]);
+        } else {
+            charSet.add(s[current++]);
+            longest = Math.max(longest, charSet.size);
+        }
+    }
+    
+    return longest;
+};
