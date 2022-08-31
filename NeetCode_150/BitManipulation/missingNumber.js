e78b4bd8ca2f37b23315b286239cd86a8fa85492
ZhouSusan <szhou089@gmail.com>
2022-08-30 21:04:16 -0700

    "refactored #268 Missing Number"

--- NeetCode_150/BitManipulation/missingNumber.js
+++ NeetCode_150/BitManipulation/missingNumber.js
@@ -10,3 +10,21 @@ var missingNumber = function(nums) {
     
     return result;
 };
+
+var missingNumber2 = function(nums) {
+
+    let fullSet = new Set();
+    
+    for (let i = 0; i <= nums.length; i++) {
+        fullSet.add(i);
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (fullSet.has(nums[i])) {
+            fullSet.delete(nums[i]);
+        }
+    }
+    
+    let missingNumber = [...fullSet.values()][0];
+    
+    return missingNumber;
+}
