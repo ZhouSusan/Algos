5121852bedc3faa00c114111da2e336a79010e6d
ZhouSusan <szhou089@gmail.com>
2022-09-08 23:26:46 -0700

    added #14 Longest Common Prefix

---
+++ leetcode/longestCommonPrefix.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+    
+    [
+    "flower",
+    "flow",
+    "flight"
+    ]
+ */
+
+    var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) {
+        return "";
+    } 
+    
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] !== strs[0][i]) {
+                return strs[0].slice(0, i); 
+            }
+        }
+    }
+    
+    return strs[0];
+};
